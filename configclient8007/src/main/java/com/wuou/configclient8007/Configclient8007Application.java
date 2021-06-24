a577a6094f3ddb27e5e3060ff87652b2ac4551c9
wuou <wuou@tal.com>
2021-06-24 18:07:33 +0800

    configClient

---
+++ configclient8007/src/main/java/com/wuou/configclient8007/Configclient8007Application.java
@@ -0,0 +1,13 @@
+package com.wuou.configclient8007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Configclient8007Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Configclient8007Application.class, args);
+    }
+
+}
---
+++ configclient8007/src/test/java/com/wuou/configclient8007/Configclient8007ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuou.configclient8007;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Configclient8007ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
