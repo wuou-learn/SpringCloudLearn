70a01566beff66a952160f2f926f75b8f792455a
wuou <wuou@tal.com>
2021-06-25 15:16:11 +0800

    configClient

---
+++ configclient8007/src/main/java/com/wuou/configclient8007/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.wuou.configclient8007.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author wuou
+ * @Date 2021/6/25 下午3:13
+ * @Version 1.0.0
+ */
+@RestController
+public class HelloController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/client/testPort")
+    public String TestPort(){
+        return "port-->"+port;
+    }
+
+}
